5b86c93b64fd762d3e0a0bdfe1034d191a683028
FlareDrakon <darrrangel@gmail.com>
2019-01-07 05:01:29 +0300

    initial structure and dependencies

---
+++ src/main/java/ru/flare/event/core/model/Task.java
@@ -0,0 +1,8 @@
+package ru.flare.event.core.model;
+
+public class Task implements Comparable<Task> {
+    @Override
+    public int compareTo(Task o) {
+        return 0;
+    }
+}
---
+++ src/test/java/ru/flare/event/core/processing/DispatchTest.java
@@ -0,0 +1,11 @@
+package ru.flare.event.core.processing;
+
+import org.junit.Test;
+
+public class DispatchTest {
+
+    @Test
+    public void TestSimpleEventPass() throws Exception {
+
+    }
+}
